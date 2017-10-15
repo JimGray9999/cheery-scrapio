db6f314bf18bb4b1bc244a3e47a84924174cd6c0
Jim Gray <JimGray9999@users.noreply.github.com>
2017-10-15 16:55:10 -0400

    modal

--- models/Note.js
+++ models/Note.js
@@ -2,4 +2,17 @@ var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
+var NoteSchema = new Schema ({
+  header: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  }
+});
+
+var Note = mongoose.model("Note", NoteSchema);
+
 exports.module = Note;
