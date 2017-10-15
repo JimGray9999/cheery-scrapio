1d9ee62d3325fd777b5a9fabb7567f013058260f
Jim Gray <JimGray9999@users.noreply.github.com>
2017-10-15 19:31:35 -0400

    updates

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
   // jQuery function so certain classes work on dynamic elements
   $("#modal1").modal();
+
+
+  $(document).on("click", "#modal1", function() {
+
+
+  });
 });
